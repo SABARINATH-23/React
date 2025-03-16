25817e04ec240990c0b9c45401d61cac4e5ab9c6
SABARINATH-23 <sabarinath0211@gmail.com>
2025-03-16 18:25:10 +0530

    Css updated

--- Facebook/src/pages/Home.jsx
+++ Facebook/src/pages/Home.jsx
@@ -6,21 +6,25 @@ const Home = ()=>{
     return(
         <>
     <div className="outer">
-    <div className="homeleft">
+      <div className="homeleft">
         <img src={logo} alt="facebook logo"></img>
          <p>FaceBook helps you connect and share with the people in your life.</p>
-    </div>
+         </div>
      <div className="homeright">
         <form action="">
+            <div className="container-1">
         <input type="text" placeholder="Email address or phone number"></input>
         <input type="password" placeholder="Password"></input>
-        <button><NavLink to={"/login"}>Login</NavLink></button>
-        <NavLink to={"/forgottenpassword"}>forgotten password?</NavLink>
-        <button><NavLink to={"/createaccount"}>Create new account</NavLink></button>
+        <button className="btn1"><NavLink to={"/login"}>Login</NavLink></button>
         <hr></hr>
-        <p><NavLink to={"/createpage"}>Create Page</NavLink>for a celebrity, brand or business.</p>
-
+        <NavLink to={"/forgottenpassword"}>forgotten password?</NavLink>
+        <button className="btn2"><NavLink to={"/createaccount"}>Create new account</NavLink></button>
+        </div>
+        <div className="container-2">
+             <p><NavLink to={"/createpage"}>Create Page</NavLink>for a celebrity, brand or business.</p>
+        </div>
         </form>
+        
          
      </div>
      </div>
