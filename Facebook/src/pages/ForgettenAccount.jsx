6d57055e19c796d8841e5b60fb1f8ca06ce16feb
SABARINATH-23 <sabarinath0211@gmail.com>
2025-03-17 16:20:33 +0530

    ForgettonPageAdded

---
+++ Facebook/src/pages/ForgettenAccount.jsx
@@ -0,0 +1,9 @@
+const ForgottenAccount = ()=>{
+    return(
+        <>
+            <h1>NavBar</h1>
+            <h1>ForGetten Account Page</h1>        
+        </>
+    )
+}
+export default ForgottenAccount;
--- Facebook/src/pages/Login.jsx
+++ Facebook/src/pages/Login.jsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router-dom";
+import logo from"../Images/4lCu2zih0ca1.svg"
+import ForgottenAccount from "./ForgettenAccount";
+
 const Login = ()=>{
     return(
         <>
-            <h1>Login Page</h1>
+            <div className="login">
+                <div className="Logintop">
+                    <img src={logo} alt="facebook logo"></img>    
+                </div>   
+                <div className="Loginbootom">
+                        <p>Log in to Facebook</p>
+                        <input type="text" placeholder="Email address or phone number"></input>
+                        <input type="password" placeholder="Password"></input>
+                        <button>Log in</button>
+                        <p><NavLink to={<ForgottenAccount/>}>Forgotten account</NavLink> <NavLink to={''}>Sign up for Facebook</NavLink></p>
+
+                </div> 
+            </div>       
         </>
     )
 }
